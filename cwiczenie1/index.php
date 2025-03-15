bc2de68dc2982bbced3399f4e9a06984cb451ce2
M4niaDaniel <emaniuel@proton.me>
2025-03-15 19:34:50 +0100

    CW1

--- cwiczenie1/index.php
+++ cwiczenie1/index.php
@@ -40,17 +40,33 @@
 			</form>
 <?php
 
-function token($dlugosc = 10) {
-    $znaki = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $znakiDlugosc = strlen($znaki);
-    $randomString = '';
-    for ($i = 0; $i < $dlugosc; $i++) {
-        $randomString .= $znaki[rand(0, $znakiDlugosc - 1)];
-    }
-    return $randomString;
-}
+	function token($dlugosc = 10) {
+		$znaki = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		$znakiDlugosc = strlen($znaki);
+		$randomString = '';
+		for ($i = 0; $i < $dlugosc; $i++) {
+			$randomString .= $znaki[rand(0, $znakiDlugosc - 1)];
+		}
+		return $randomString;
+	}
 
-echo "<p>Jednorazowy token dostępu do skarbca: " . token(10) . "</p>";
+	//echo "<p>Jednorazowy token dostępu do skarbca: " . token(10) . "</p>";
+
+	if(isset($_POST['login'], $_POST['haslo'])){
+
+		$login = $_POST['login'];
+		$password = $_POST['haslo'];
+
+		if($login == 'pawlo' && $password == 'napadnabank'){
+			echo'WEŹ SIE DO ROBOTY PAWEL NIEROBIE I ROBAKU JEDEN!';
+			echo "<p>Jednorazowy token dostępu do skarbca: " . token(10) . "</p>";
+		}elseif($login == 'gawo'&& $password == 'likeaboss'){
+			echo 'WITAJ GAWEŁ SZEFIE <i>WSZYSTKICH</i> SZEFUF';
+			echo "<p>Jednorazowy token dostępu do skarbca: " . token(10) . "</p>";
+		}else{
+			echo 'NIEPOPRAWNY LOGIN I/LUB HASLO!';
+		}
+	}
 
 ?>
 
