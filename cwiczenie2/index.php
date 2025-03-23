81142462066eee9a468c2d98fb45ca6626c6955e
M4niaDaniel <emaniuel@proton.me>
2025-03-23 12:50:44 +0100

    cw2 form

--- cwiczenie2/index.php
+++ cwiczenie2/index.php
@@ -1,33 +1,30 @@
 <!DOCTYPE html>
 <html lang="pl">
-
-<head>
-
-	<meta charset="utf-8">
-	<title>2 - Obsługa formularzy w PHP</title>
-	<link href="arkusz-cwiczenia.css" rel="stylesheet">
-	
-</head>
-
-<body>
-
-	<main>
-	
-		<article>
-
-			<header>
-			
-				<h1>Zamów korepetycje online</h1>
-
-			</header>
-			
-			
-			
-			
-			
-		</article>
+	<head>
+		<meta charset="utf-8">
+		<title>2 - Obsługa formularzy w PHP</title>
+		<link href="arkusz-cwiczenia.css" rel="stylesheet">
 		
-	</main>
-	
-</body>
+	</head>
+	<body>
+		<main>
+			<article>
+				<header>
+					<h1>Zamów korepetycje online</h1>
+				</header>
+				<form >
+					<input type="text" name="name" id="name" required placeholder="Wpisz imię...">
+					<select>
+						<option value="pol">Język Polski(50)</option>
+						<option value="his">Historia(60)</option>
+						<option value="wos">Wiedza O Społeczeństwie(45)</option>
+					</select>
+					<input type="number" name="num" id="num">
+					<Label for="notes">Notatki(5)</Label>
+					<input type="checkbox" name="notes" id="notes">
+					<input type="submit" value="Zamów Korepetycje">
+				</form>
+			</article>	
+		</main>	
+	</body>
 </html>
--- cwiczenie2/korepetycje.php
+++ cwiczenie2/korepetycje.php
@@ -1,35 +1,24 @@
 <!DOCTYPE html>
 <html lang="pl">
+	<head>
 
-<head>
-
-	<meta charset="utf-8">
-	<title>2 - Obsługa formularzy w PHP</title>
-	<link href="arkusz-cwiczenia.css" rel="stylesheet">
-	
-</head>
-
-<body>
-
-	<main>
-	
-		<article>
-
-			<header>
-			
-				<h1>Podsumowanie zamówienia korepetycji</h1>
-
-			</header>
-			
-<?php			
-			
-
-
-			
-?>			
-		</article>
+		<meta charset="utf-8">
+		<title>2 - Obsługa formularzy w PHP</title>
+		<link href="arkusz-cwiczenia.css" rel="stylesheet">
 		
-	</main>
+	</head>
+	<body>
+		<main>	
+			<article>
+				<header>
+					<h1>Podsumowanie zamówienia korepetycji</h1>
+				</header>
+				
+				<?php			
+							
+				?>			
+			</article>		
+		</main>
 	
-</body>
+	</body>
 </html>
